c0623777bc571a3d40e6440dad8d60c973fbf5a3
piotrek1543 <piotrekert90@gmail.com>
2015-11-09 23:50:34 +0100

    Rearranging test code for a pull request

--- app/src/androidTest/java/com/google/samples/apps/topeka/activity/SignInActivityTest.java
+++ app/src/androidTest/java/com/google/samples/apps/topeka/activity/SignInActivityTest.java
@@ -29,7 +29,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -37,12 +36,14 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static java.lang.String.valueOf;
-import static org.hamcrest.Matchers.anything;
+import static com.google.samples.apps.topeka.R.id.last_initial;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -71,38 +72,142 @@ public class SignInActivityTest {
     }
 
     @Test
-    public void signIn_checkIfEditTextViewsAreInitiallyEmpty() {
+    public void signIn_checkIfFirstNameEditTextIsInitiallyEmpty() {
         onView(withId(R.id.first_name)).check(matches(withText(isEmptyOrNullString())));
-        onView(withId(R.id.last_initial)).check(matches(withText(isEmptyOrNullString())));
+    }
+
+    @Test
+    public void signIn_checkIfLastInitialEditTextIsInitiallyEmpty() {
+        onView(withId(last_initial)).check(matches(withText(isEmptyOrNullString())));
     }
 
     @Test
     public void signIn_performSuccessful() {
         inputData();
         onView(withId(R.id.done)).check(matches(isDisplayed())).perform(click());
-        onView(withId(R.id.toolbar_player)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void signIn_chooseAvatar() {
-        for (int i = 0; i < 16; i++) {
-            onData(anything())
-                    .inAdapterView(withId(R.id.avatars))
-                    .atPosition(15)
-                    .check(matches(isDisplayed()))
-                    .perform(click()).check(matches(isChecked()));
-        }
+    public void signIn_chooseAvatar_01() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 01")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_02() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 11")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_03() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 21")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_04() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 31")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_05() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 41")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_06() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 51")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_07() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 61")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_08() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 71")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_09() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 81")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_10() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 91")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_11() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 101")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_12() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 111")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_13() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 121")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_14() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 131")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_15() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 141")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
+    }
+
+    @Test
+    public void signIn_chooseAvatar_16() {
+        onView(allOf(withParent(withId(R.id.avatars)), withContentDescription("Avatar 151")))
+                .check(matches(isDisplayed()))
+                .perform(click()).check(matches(isChecked()));
     }
 
     @Test
     public void signIn_withLongLastName() {
         inputData();
-        onView(withId(R.id.last_initial)).perform(typeText(valueOf(R.string.first_name)), closeSoftKeyboard());
-        onView(withId(R.id.last_initial)).check(matches(withText(TEST_LAST_INITIAL)));
+        onView(withId(last_initial)).perform(typeText("somelongtext"), closeSoftKeyboard());
+        onView(withId(last_initial)).check(matches(withText(TEST_LAST_INITIAL)));
     }
 
     private void inputData() {
         onView(withId(R.id.first_name)).perform(typeText(TEST_FIRST_NAME), closeSoftKeyboard());
-        onView(withId(R.id.last_initial)).perform(typeText(TEST_LAST_INITIAL), closeSoftKeyboard());
+        onView(withId(last_initial)).perform(typeText(TEST_LAST_INITIAL), closeSoftKeyboard());
     }
 }
